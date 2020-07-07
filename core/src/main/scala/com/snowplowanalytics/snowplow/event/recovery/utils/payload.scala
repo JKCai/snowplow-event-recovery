824ae7ae55cdd374003e4d28e106df9a9ffaae94
Piotr Limanowski <piotr@codearsonist.com>
2020-07-07 14:29:57 +0200

    Add support for CPFormatViolation bad rows (closes #59)
    In rare cases, when `CPFormatViolation` is caused by a possibly-reversible malformed query string.
    Given `a=b&c={d}`, original payload can be recovered by:
    - extracting the querystring
    - stripping invalid characters
    - replacing original querystring
    Resulting in a payload with querystring in compatible format, ie. `a=b&c=d`.

--- core/src/main/scala/com/snowplowanalytics/snowplow/event/recovery/utils/payload.scala
+++ core/src/main/scala/com/snowplowanalytics/snowplow/event/recovery/utils/payload.scala
@@ -19,12 +19,15 @@ package util
 import java.net.URLEncoder
 import java.nio.charset.StandardCharsets.UTF_8
 import scala.collection.JavaConverters._
-import cats._
 import cats.instances.list._
 import cats.instances.string._
+import cats.syntax.either._
 import domain._
 import badrows._
 import CollectorPayload.thrift.model1.CollectorPayload
+import org.joda.time.DateTime
+import java.util.UUID
+import java.net.URLDecoder
 
 object payload {
 
@@ -39,7 +42,7 @@ object payload {
       )
       cp.userAgent     = p.useragent.orNull
       cp.refererUri    = p.refererUri.orNull
-      cp.querystring   = Foldable[List].foldMap(p.querystring)(_.value.getOrElse(""))
+      cp.querystring   = toQuerystring(p.querystring)
       cp.body          = p.body.orNull
       cp.headers       = p.headers.asJava
       cp.contentType   = p.contentType.orNull
@@ -82,4 +85,49 @@ object payload {
 
   private[this] val thriftSchema =
     "iglu:com.snowplowanalytics.snowplow/CollectorPayload/thrift/1-0-0"
+
+  private[this] val cocoercePF: PartialFunction[CollectorPayload, Payload.CollectorPayload] = {
+    case cp: CollectorPayload => {
+      val vendorVersion = toVendorVersion(cp.path)
+      Payload.CollectorPayload(
+        vendor        = vendorVersion.vendor,
+        version       = vendorVersion.version,
+        querystring   = fromQuerystring(cp.querystring),
+        contentType   = Option(cp.contentType),
+        body          = Option(cp.body),
+        collector     = cp.collector,
+        encoding      = cp.encoding,
+        hostname      = Option(cp.hostname),
+        timestamp     = Option(cp.timestamp).map(new DateTime(_)),
+        ipAddress     = Option(cp.ipAddress),
+        useragent     = Option(cp.userAgent),
+        refererUri    = Option(cp.refererUri),
+        headers       = Either.catchNonFatal(cp.headers.asScala.toList).getOrElse(List.empty),
+        networkUserId = Option(cp.networkUserId).map(UUID.fromString)
+      )
+    }
+  }
+
+  private[this] case class VendorVersion(vendor: String, version: String)
+
+  private[this] val toVendorVersion = (path: String) =>
+    path.split("/").toList match {
+      case vendor :: version :: Nil => VendorVersion(vendor, version)
+      case _                        => VendorVersion("unknown", "unknown")
+    }
+
+  private[this] val fromQuerystring = (s: String) => {
+    val urlDecoded = (s: String) => URLDecoder.decode(s, UTF_8.toString())
+    val toNVP = (s: String) =>
+      s.split("=").toList match {
+        case name :: value :: Nil => List(NVP(name, Some(urlDecoded(value))))
+        case name :: Nil          => List(NVP(name, None))
+        case _                    => List.empty
+      }
+
+    s.split("&").toList.flatMap(toNVP)
+  }
+
+  val cocoerce: CollectorPayload => Recovering[Payload.CollectorPayload] =
+    (a: CollectorPayload) => cocoercePF.lift(a).toRight(UncocoerciblePayload(a.toString))
 }
