069f355d351ef06df6bdf3ee68d3fb0da2f5f014
Łukasz Indykiewicz <lukasz.indykiewicz@gmail.com>
2020-07-09 02:34:38 +0200

    Add property tests for coerce/cocoerce

--- core/src/main/scala/com/snowplowanalytics/snowplow/event/recovery/utils/payload.scala
+++ core/src/main/scala/com/snowplowanalytics/snowplow/event/recovery/utils/payload.scala
@@ -19,8 +19,6 @@ package util
 import java.net.URLEncoder
 import java.nio.charset.StandardCharsets.UTF_8
 import scala.collection.JavaConverters._
-import cats.instances.list._
-import cats.instances.string._
 import cats.syntax.either._
 import domain._
 import badrows._
@@ -40,6 +38,7 @@ object payload {
         p.encoding,
         p.collector
       )
+      cp.path          = mkPath(p.vendor, p.version)
       cp.userAgent     = p.useragent.orNull
       cp.refererUri    = p.refererUri.orNull
       cp.querystring   = toQuerystring(p.querystring)
@@ -58,7 +57,7 @@ object payload {
         p.encoding,
         p.loaderName
       )
-      cp.path          = s"${p.vendor}/${p.version}"
+      cp.path          = mkPath(p.vendor, p.version)
       cp.userAgent     = p.useragent.orNull
       cp.refererUri    = p.refererUri.orNull
       cp.querystring   = toQuerystring(p.parameters)
@@ -67,6 +66,8 @@ object payload {
       cp
     }
   }
+
+  private[this] val mkPath = (vendor: String, version: String) => s"/$vendor/$version"
 
   /**
     * A homomorphic transformation from a `Payload` of a known-type to `CollectorPayload`.
@@ -78,7 +79,7 @@ object payload {
 
   private[this] val toQuerystring = (l: List[NVP]) => {
     val urlEncode = (str: String) => URLEncoder.encode(str, UTF_8.toString)
-    val show      = (n: NVP) => n.value.map(urlEncode).map(v => s"${n.name}=$v").getOrElse("")
+    val show      = (n: NVP) => s"${n.name}=${urlEncode(n.value.getOrElse(""))}"
 
     l.map(show).mkString("&")
   }
@@ -95,10 +96,10 @@ object payload {
         querystring   = fromQuerystring(cp.querystring),
         contentType   = Option(cp.contentType),
         body          = Option(cp.body),
-        collector     = cp.collector,
-        encoding      = cp.encoding,
+        collector     = Option(cp.collector).getOrElse(""),
+        encoding      = Option(cp.encoding).getOrElse(""),
         hostname      = Option(cp.hostname),
-        timestamp     = Option(cp.timestamp).map(new DateTime(_)),
+        timestamp     = Option(cp.timestamp).filterNot(_ == 0L).map(new DateTime(_)),
         ipAddress     = Option(cp.ipAddress),
         useragent     = Option(cp.userAgent),
         refererUri    = Option(cp.refererUri),
@@ -110,22 +111,36 @@ object payload {
 
   private[this] case class VendorVersion(vendor: String, version: String)
 
-  private[this] val toVendorVersion = (path: String) =>
-    path.split("/").toList match {
-      case vendor :: version :: Nil => VendorVersion(vendor, version)
-      case _                        => VendorVersion("unknown", "unknown")
-    }
+  private[this] val toVendorVersion = (path: String) => {
+    val Empty = VendorVersion("", "")
+    val split = (s: String) =>
+      s.split("/").toList match {
+        case _ :: "" :: version :: Nil     => VendorVersion("", version)
+        case _ :: vendor :: Nil            => VendorVersion(vendor, "")
+        case _ :: vendor :: version :: Nil => VendorVersion(vendor, version)
+        case _                             => Empty
+      }
+
+    Option(path).map(split).getOrElse(Empty)
+  }
 
   private[this] val fromQuerystring = (s: String) => {
+    val Empty      = List.empty
     val urlDecoded = (s: String) => URLDecoder.decode(s, UTF_8.toString())
     val toNVP = (s: String) =>
       s.split("=").toList match {
-        case name :: value :: Nil => List(NVP(name, Some(urlDecoded(value))))
+        case Nil                  => List(NVP("", Some("")))
+        case "" :: "" :: Nil      => List(NVP("", Some("")))
+        case "" :: value :: Nil   => List(NVP("", Some(urlDecoded(value))))
+        case name :: "" :: Nil    => List(NVP(name, None))
+        case "" :: Nil            => Empty
         case name :: Nil          => List(NVP(name, None))
-        case _                    => List.empty
+        case name :: value :: Nil => List(NVP(name, Some(urlDecoded(value))))
+        case _                    => Empty
       }
+    val split = (s: String) => s.split("&").toList.flatMap(toNVP)
 
-    s.split("&").toList.flatMap(toNVP)
+    Option(s).map(split).getOrElse(Empty)
   }
 
   val cocoerce: CollectorPayload => Recovering[Payload.CollectorPayload] =
